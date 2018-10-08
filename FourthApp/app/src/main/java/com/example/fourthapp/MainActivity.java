0f34fc258db99bede7b40066dff0087c3b94c47d
WFrame0224 <wf0224@outlook.com>
2018-10-08 15:21:22 +0800

    增加了FrameLayout的例程，尝试了三层页面的使用

--- FourthApp/app/src/main/java/com/example/fourthapp/MainActivity.java
+++ FourthApp/app/src/main/java/com/example/fourthapp/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.fourthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void openFrameLayout(View view){
+        // 创建Intent对象，并调用另一个窗口，显示Intent，明确指明了需要启动和触发的组件类名
+        Intent intent = new Intent(this,FrameLayoutDemo.class);
+        startActivity(intent);
     }
 }
---
+++ FourthApp/app/src/main/java/com/example/fourthapp/TestThirdFloorActivity.java
@@ -0,0 +1,12 @@
+package com.example.fourthapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestThirdFloorActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_third_floor_activity);
+    }
+}
