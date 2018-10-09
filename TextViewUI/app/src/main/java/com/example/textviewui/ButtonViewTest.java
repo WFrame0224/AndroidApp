c112335f7ea6bd8984d874ed55813a58432eb3f2
WFrame0224 <wf0224@outlook.com>
2018-10-09 17:21:15 +0800

    完成了EditView的演示，以及Button的演示

---
+++ TextViewUI/app/src/main/java/com/example/textviewui/ButtonViewTest.java
@@ -0,0 +1,12 @@
+package com.example.textviewui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ButtonViewTest extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_activity);
+    }
+}
---
+++ TextViewUI/app/src/main/java/com/example/textviewui/EditTextTest.java
@@ -0,0 +1,12 @@
+package com.example.textviewui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditTextTest extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_text_activity);
+    }
+}
