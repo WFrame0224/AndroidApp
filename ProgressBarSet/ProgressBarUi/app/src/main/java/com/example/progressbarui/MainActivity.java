db4b9853bbec417ed6dd039ef14cb6b50cf407c8
WFrame0224 <wf0224@outlook.com>
2018-10-18 21:53:16 +0800

    增加了ProgressBar基础组件的演示

---
+++ ProgressBarSet/ProgressBarUi/app/src/main/java/com/example/progressbarui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.progressbarui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 该程序
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
