5215301c3a6f44702e7d16bcb1133b290cd35a85
WFrame0224 <wf0224@outlook.com>
2018-10-27 10:57:29 +0800

    修改了一些内容，但是还是闪退，尚未找到原因

--- ActionBarSet/ActionBarSwipNavUi/app/src/main/java/com/example/actionbarswipnavui/DummyFragment.java
+++ ActionBarSet/ActionBarSwipNavUi/app/src/main/java/com/example/actionbarswipnavui/DummyFragment.java
@@ -2,7 +2,6 @@ package com.example.actionbarswipnavui;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
